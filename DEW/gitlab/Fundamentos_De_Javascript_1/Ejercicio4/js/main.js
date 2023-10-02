623e68ec4859c8747b44e3462e6f5995ec666783
Rodrigo <rodrigo18sa@gmail.com>
2023-10-02 13:59:14 +0100

    estoy enviando parte el ejercicio 4 de fundamentos de javascript

--- DEW/gitlab/Fundamentos_De_Javascript_1/Ejercicio4/js/main.js
+++ DEW/gitlab/Fundamentos_De_Javascript_1/Ejercicio4/js/main.js
@@ -7,13 +7,26 @@ const DOM = {
 
 const obtenerNota = (e) =>{ 
     e.preventDefault()
+
+    const childrenMensaje=DOM.mensaje.children;
+    [...childrenMensaje].every(child=>{
+        DOM.mensaje.removeChild(child)
+    })
+
     const nota=DOM.nota.value;
-    if (nota>=0 && nota<=5) {    
+    if (nota>=0 && nota<5) {    
         const strong=document.createElement("strong");
         strong.style="color:red;"
         strong.textContent="Suspenso"
         DOM.mensaje.appendChild(strong);
     }
+    if (nota>=5 && nota<7) {    
+        const strong=document.createElement("strong");
+        strong.style="color:blue;"
+        strong.textContent="Aprobado"
+        DOM.mensaje.appendChild(strong);
+    }
+
 }
 DOM.form1.addEventListener("submit",obtenerNota)
  
