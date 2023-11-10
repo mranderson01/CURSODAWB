28f7cf48b31114c4d03e86fbf03c717801e38d16
Rodrigo <rodrigo18sa@gmail.com>
2023-11-10 11:55:40 +0000

    mejorando index de app gimnasio

---
+++ DOR/652NMB 23-24 DOR - Diseño de interfaces web/js/index.js
@@ -0,0 +1,18 @@
+const DOM ={
+
+}
+
+/*
+    NOCHE
+    colores:
+    blue Dark: #021C1E
+    Cadet blue: #004445
+    rain: #2C7873
+    greenery; #6FB98F
+
+    DIA
+    Soft gray: #DDDEDE
+    blacksifh: #232122
+    houseplant: #A5C05B
+    blue-gray:  # 7BA4A8
+*/
